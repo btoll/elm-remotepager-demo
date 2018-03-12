05058df69819c9552ff4ccad7a113d6b0399bc35
Benjamin Toll <benjam72@yahoo.com>
2018-03-12 18:40:12 -0400

    Add new type and decoder to Data.Hacker to work with Pager

--- client/src/Data/Hacker.elm
+++ client/src/Data/Hacker.elm
@@ -1,5 +1,6 @@
-module Data.Hacker exposing (Hacker, decoder, encoder, manyDecoder, new, succeed)
+module Data.Hacker exposing (Hacker, HackerWithPager, decoder, encoder, manyDecoder, new, pagingDecoder, succeed)
 
+import Data.Pager
 import Json.Decode as Decode exposing (Decoder, float, int, list, string)
 import Json.Decode.Pipeline exposing (decode, optional, required)
 import Json.Encode as Encode
@@ -10,6 +11,13 @@ type alias Hacker =
     { id : Int
     , name : String
     }
+
+
+type alias HackerWithPager =
+    { hackers : List Hacker
+    , pager : Data.Pager.Pager
+    }
+
 
 
 new : Hacker
@@ -37,6 +45,14 @@ encoder hacker =
         [ ( "id", Encode.int hacker.id )
         , ( "name", Encode.string hacker.name )
         ]
+
+
+pagingDecoder : Decoder HackerWithPager
+pagingDecoder =
+    decode HackerWithPager
+        |> required "hackers" manyDecoder
+        |> required "pager" Data.Pager.decoder
+
 
 succeed : a -> Decoder a
 succeed =
